656bc3c8e2ff7b5f8576f436a6e0c3ee88ffce1c
mike <fantazuor@gmail.com>
2015-09-01 11:16:08 +0300

    make network actor and service actor router

--- src/main/scala/truerss/system/SystemActor.scala
+++ src/main/scala/truerss/system/SystemActor.scala
@@ -1,30 +1,33 @@
 package truerss.system
 
-import akka.actor.{Actor, ActorLogging, Props}
+import akka.actor.{Actor, Props}
 import akka.io.IO
+import akka.routing.SmallestMailboxPool
 
-import scala.slick.jdbc.JdbcBackend.DatabaseDef
 import spray.can.Http
+import scala.slick.jdbc.JdbcBackend.DatabaseDef
 
-import truerss.api.{WSApi, RoutingService}
+import truerss.api.{RoutingService, WSApi}
 import truerss.db.DbActor
 import truerss.models.CurrentDriver
 
 /**
  * Created by mike on 2.8.15.
  */
-class SystemActor(dbDef: DatabaseDef, driver: CurrentDriver) extends Actor
-  with ActorLogging {
+class SystemActor(dbDef: DatabaseDef, driver: CurrentDriver) extends Actor {
 
   implicit val system = context.system
 
   val dbRef = context.actorOf(Props(new DbActor(dbDef, driver)), "db")
 
-  val networkRef = context.actorOf(Props(new NetworkActor), "network")
+  val networkRef = context.actorOf(
+    Props(new NetworkActor).withRouter(SmallestMailboxPool(10)), "network-router")
 
   val sourcesRef = context.actorOf(Props(new SourcesActor(self, networkRef)), "sources")
 
-  val proxyRef = context.actorOf(Props(new ProxyServiceActor(dbRef, networkRef, sourcesRef)), "proxy")
+  val proxyRef = context.actorOf(Props(
+    new ProxyServiceActor(dbRef, networkRef, sourcesRef))
+      .withRouter(SmallestMailboxPool(10)), "service-router")
 
   val api = context.actorOf(Props(new RoutingService(proxyRef)), "api")
 
