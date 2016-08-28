f0c2e1b57de7e2b7de5dbf90002cfd05e8ca264e
mike <mike.fch1@gmail.com>
2016-08-28 10:52:31 +0300

    version bump

--- project/Truerssbuild.scala
+++ project/Truerssbuild.scala
@@ -40,7 +40,7 @@ object Truerssbuild extends Build {
       (compile in Compile) <<= (compile in Compile).dependsOn(buildCoffee),
       organization := "net.truerss",
       name := "truerss",
-      version := "0.0.2",
+      version := "0.0.2.1",
       parallelExecution in Test := false,
       assemblyJarName in assembly := s"truerss-${version.value}.jar",
       mainClass in assembly := Some("truerss.Boot"),
