856efe174a2a770f5c69e1a1e59db372f96be4e1
mike <mike.fch1@gmail.com>
2016-12-17 22:24:23 +0300

    run http

--- src/main/scala/truerss/system/SystemActor.scala
+++ src/main/scala/truerss/system/SystemActor.scala
@@ -1,32 +1,31 @@
 package truerss.system
 
-import akka.actor.{Actor, ActorLogging, Props, OneForOneStrategy}
 import akka.actor.SupervisorStrategy._
-import akka.io.IO
-import akka.routing.SmallestMailboxPool
+import akka.actor.{Actor, ActorLogging, OneForOneStrategy, Props}
+import akka.http.scaladsl.Http
 import akka.pattern.gracefulStop
-
-import spray.can.Http
-
-import truerss.api.{RoutingService, WSApi}
-import truerss.config.TrueRSSConfig
-import truerss.db.{SupportedDb, DbActor}
-import truerss.models.CurrentDriver
-import truerss.system.util.{NotifyLevels, Notify}
-
+import akka.stream.ActorMaterializer
 import slick.jdbc.JdbcBackend.DatabaseDef
-import scala.concurrent.duration._
+import truerss.api.{RoutingApiImpl, RoutingService, WSApi}
+import truerss.config.TrueRSSConfig
+import truerss.db.{DbActor, SupportedDb}
+import truerss.models.CurrentDriver
+import truerss.system.util.{Notify, NotifyLevels}
+
 import scala.concurrent.Future
+import scala.concurrent.duration._
 
 class SystemActor(config: TrueRSSConfig,
                   dbDef: DatabaseDef,
                   driver: CurrentDriver,
-                  backend: SupportedDb) extends Actor with ActorLogging {
+                  backend: SupportedDb)
+  extends Actor with ActorLogging {
 
-  import global._
   import context.dispatcher
+  import global._
 
   implicit val system = context.system
+  implicit val materializer = ActorMaterializer()
 
   override val supervisorStrategy =
     OneForOneStrategy(maxNrOfRetries = 10, withinTimeRange = 1 minute) {
@@ -60,7 +59,7 @@ class SystemActor(config: TrueRSSConfig,
 
   val socketApi = context.actorOf(Props(classOf[WSApi], config.wsPort), "ws-api")
 
-  IO(Http) ! Http.Bind(api, interface = config.host, port = config.port)
+  Http().bindAndHandle(new RoutingApiImpl(proxyRef).route, config.host, config.port)
 
   def stopChildren = Future.sequence(Vector(
     dbRef, sourcesRef, proxyRef, api, socketApi
@@ -77,9 +76,6 @@ class SystemActor(config: TrueRSSConfig,
     case StopSystem =>
       log.info("Stop actor system")
       stopChildren.map(_ => system.terminate())
-
-    case Http.Bound(isa) =>
-      log.info(s"Run API on ${isa.getPort} port")
 
     case x => proxyRef forward x
   }
