fdd2e1d56676a19bd8c7d21de42c5a7b4d40c180
mike <mike.fch1@gmail.com>
2020-07-13 00:30:09 +0300

    content reader refactoring

--- src/main/scala/truerss/services/ContentReaderService.scala
+++ src/main/scala/truerss/services/ContentReaderService.scala
@@ -2,8 +2,8 @@ package truerss.services
 
 import java.net.URL
 
-import com.github.truerss.base.{ContentTypeParam, Errors}
 import com.github.truerss.base.aliases.WithContent
+import com.github.truerss.base.{ContentTypeParam, Errors}
 import org.jsoup.Jsoup
 import org.slf4j.LoggerFactory
 import truerss.db.Predefined
@@ -11,8 +11,7 @@ import truerss.dto.{FeedDto, SetupKey}
 import truerss.util.{Request, syntax}
 import zio.Task
 
-import scala.concurrent.{ExecutionContext, Future}
-import scala.util.{Failure, Success, Try}
+import scala.concurrent.ExecutionContext
 
 class ContentReaderService(
                             feedsService: FeedsService,
@@ -21,7 +20,6 @@ class ContentReaderService(
                           )(implicit ec: ExecutionContext) {
 
   import ContentReaderService._
-  import syntax.future._
   import syntax.ext._
 
   protected val logger = LoggerFactory.getLogger(getClass)
@@ -31,7 +29,7 @@ class ContentReaderService(
       case Some(_) => Task.succeed(ReadResult(feed))
       case None =>
         if (forceReadContent) {
-          Task.fromFuture { implicit ec => processContent(feedId, feed) }
+          processContent(feedId, feed)
         } else {
           for {
             setup <- Task.fromFuture { implicit ec =>
@@ -41,46 +39,46 @@ class ContentReaderService(
             result <- if (setup.value) {
               Task.succeed(ReadResult(feed))
             } else {
-              Task.fromFuture { implicit ec =>
-                processContent(feedId, feed)
-              }
+              processContent(feedId, feed)
             }
           } yield result
         }
     }
   }
 
-  private def processContent(feedId: Long, feed: FeedDto): Future[ReadResult] = {
+  private def processContent(feedId: Long, feed: FeedDto): Task[ReadResult] = {
     logger.debug(s"Need to read content for $feedId")
-    read(feed.url).fold(
-      error => {
+
+    read(feed.url).flatMap {
+      case Left(error) =>
         logger.warn(s"Failed to fetch content: $error for the feed: $feedId")
-        ReadResult(error.some, feed).toF
-      },
-      contentOpt => {
-        contentOpt.map { content =>
-          feedsService.updateContent(feedId, content)
-          ReadResult(feed.copy(content = Some(content))).toF
-        }.getOrElse(ReadResult(feed).toF)
-      }
-    )
+        Task.succeed(ReadResult(error.some, feed))
+      case Right(contentOpt) =>
+        contentOpt match {
+          case Some(content) =>
+            for {
+              _ <- feedsService.updateContent(feedId, content)
+            } yield ReadResult(feed.copy(content = Some(content)))
+          case None =>
+            Task.succeed(ReadResult(feed))
+        }
+
+    }
   }
 
-  protected def read(url: String): Either[String, Option[String]] = {
+  protected def read(url: String): Task[Either[String, Option[String]]] = {
     val tmp = new URL(url)
     val c = applicationPluginsService.getContentReaderOrDefault(tmp)
       .asInstanceOf[WithContent]
 
     if (c.needUrl) {
-      pass(c.content(ContentTypeParam.UrlRequest(tmp)))
-    } else {
-      extractContent(url) match {
-        case Success(value) =>
-          pass(c.content(ContentTypeParam.HtmlRequest(value)))
-
-        case Failure(exception) =>
-          Left(exception.getMessage)
+      Task {
+        pass(c.content(ContentTypeParam.UrlRequest(tmp)))
       }
+    } else {
+      for {
+        value <- extractContent(url)
+      } yield pass(c.content(ContentTypeParam.HtmlRequest(value)))
     }
   }
 
@@ -102,14 +100,13 @@ object ContentReaderService {
     }
   }
 
-  private def extractContent(url: String): Try[String] = {
-    val response = Request.getResponse(url)
-
-    if (response.isError) {
-      Failure(new RuntimeException(s"Connection error for $url"))
-    } else {
-      Success(Jsoup.parse(response.body).body().html())
-    }
+  private def extractContent(url: String): Task[String] = {
+    // todo blocking thread ?
+    for {
+      response <- Task(Request.getResponse(url))
+      _ <- Task.fail(ContentReadError(s"Connection error for $url")).when(response.isError)
+      result <- Task(Jsoup.parse(response.body).body().html())
+    } yield result
   }
 
   private def pass(result: Either[Errors.Error, Option[String]]): Either[String, Option[String]] = {
--- src/main/scala/truerss/services/ValidationError.scala
+++ src/main/scala/truerss/services/ValidationError.scala
@@ -2,4 +2,5 @@ package truerss.services
 
 case class ValidationError(errors: List[String]) //extends Throwable
 
+case class ContentReadError(error: String) extends Throwable
 
