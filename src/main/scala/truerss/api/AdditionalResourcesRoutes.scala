0a800f6c5e9f301e922733672193e9de708ae980
mike <mike.fch1@gmail.com>
2020-08-01 22:28:58 +0300

    handle redirects

---
+++ src/main/scala/truerss/api/AdditionalResourcesRoutes.scala
@@ -0,0 +1,70 @@
+package truerss.api
+
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.headers.HttpCookie
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.server.Route
+
+import scala.io.Source
+
+class AdditionalResourcesRoutes(private val wsPort: Int) {
+
+  import AdditionalResourcesRoutes._
+
+  protected val fileName = "index.html"
+
+  val route = pathEndOrSingleSlash {
+    index
+  }~ pathPrefix("about" | "settings" | "plugins" | "favorites" | "show" | "search") {
+    extractMatchedPath { path =>
+      extractUnmatchedPath { unmatched =>
+        handleXHR(
+          () => complete(about),
+          () => index(s"$path$unmatched")
+        )
+      }
+    }
+  }
+
+  private def handleXHR(
+                 xhr: () => Route,
+                 notXhr: () => Route
+               ) = optionalHeaderValueByName(xhrHeaderName) {
+    case Some(value) if value == xhrHeaderValue =>
+      xhr()
+
+    case _ =>
+      notXhr()
+  }
+
+  private def index: Route = index(None)
+  private def index(str: String): Route = index(Some(str))
+
+  private def index(redirectTo: Option[String]): Route = {
+    val more = redirectTo.map { x => Seq(HttpCookie(redirectToC, x)) }.getOrElse(Seq.empty)
+    setCookie(HttpCookie(portC, s"$wsPort"), more : _*) {
+      complete {
+        HttpEntity(
+          ContentTypes.`text/html(UTF-8)`,
+          Source.fromInputStream(getClass.getResourceAsStream(s"/$fileName")).mkString
+        )
+      }
+    }
+  }
+}
+
+object AdditionalResourcesRoutes {
+
+  final val portC = "port"
+  final val redirectToC = "redirectTo"
+  final val xhrHeaderName =  "X-Requested-With"
+  final val xhrHeaderValue = "XMLHttpRequest".toLowerCase
+
+  final val about =
+    """
+          TrueRss is an open-source feed reader with a customizable plugin system for any content (atom, RSS, youtube channels...).
+          More info <a href='https://github.com/truerss/truerss'>Github page</a>.
+          <br/>
+          Download plugins: <a href='https://github.com/truerss/plugins/releases'>plugins</a>
+    """.stripMargin
+}
--- src/main/scala/truerss/api/RoutingEndpoint.scala
+++ src/main/scala/truerss/api/RoutingEndpoint.scala
@@ -1,15 +1,11 @@
 package truerss.api
 
-import akka.http.scaladsl.model.headers.{HttpCookie, RawHeader}
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.directives.LoggingMagnet
 import akka.stream.Materializer
-import truerss.services._
-
 import org.slf4j.LoggerFactory
-
-import scala.io.Source
+import truerss.services._
 
 class RoutingEndpoint(
                        feedsService: FeedsService,
@@ -24,8 +20,6 @@ class RoutingEndpoint(
                        markService: MarkService,
                        wsPort: Int
                      )(implicit val materializer: Materializer) {
-
-  import RoutingEndpoint._
 
   val sourcesApi = new SourcesApi(feedsService, sourcesService)
   val feedsApi = new FeedsApi(feedsService, contentReaderService)
@@ -56,22 +50,9 @@ class RoutingEndpoint(
     markApi.route ~
     sourcesOverviewApi.route
 
-  val fileName = "index.html"
+  val additional = new AdditionalResourcesRoutes(wsPort)
 
-  val route = pathEndOrSingleSlash {
-    setCookie(HttpCookie("port", s"$wsPort")) {
-      complete {
-        HttpEntity(
-          ContentTypes.`text/html(UTF-8)`,
-          Source.fromInputStream(getClass.getResourceAsStream(s"/$fileName")).mkString
-        )
-      }
-    }
-  } ~ apis ~ pathPrefix("about") {
-    complete {
-      about
-    }
-  } ~ pathPrefix("css") {
+  val route = additional.route ~ apis ~ pathPrefix("css") {
     getFromResourceDirectory("css")
   } ~
     pathPrefix("js") {
@@ -82,29 +63,8 @@ class RoutingEndpoint(
     } ~
     pathPrefix("templates") {
       getFromResourceDirectory("templates")
-    } ~ pathPrefix("show" / Segments) { segments =>
-    respondWithHeader(makeRedirect(s"/show/${segments.mkString("/")}")) {
-      redirect("/", StatusCodes.Found)
     }
-  } ~ pathPrefix(Segment) { segment =>
-    respondWithHeader(makeRedirect(segment)) {
-      redirect("/", StatusCodes.Found)
-    }
-  }
 
 
-}
 
-object RoutingEndpoint {
-  def makeRedirect(location: String) = {
-    RawHeader("Redirect", location)
-  }
-
-  final val about =
-    """
-          TrueRss is an open-source feed reader with a customizable plugin system for any content (atom, RSS, youtube channels...).
-          More info <a href='https://github.com/truerss/truerss'>Github page</a>.
-          <br/>
-          Download plugins: <a href='https://github.com/truerss/plugins/releases'>plugins</a>
-    """.stripMargin
 }
