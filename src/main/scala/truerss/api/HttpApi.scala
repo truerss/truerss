14467da0c21532b92e202c490bc8885b447d3189
mike <mike.fch1@gmail.com>
2021-05-25 00:05:49 +0300

    update

--- src/main/scala/truerss/api/HttpApi.scala
+++ src/main/scala/truerss/api/HttpApi.scala
@@ -1,7 +1,7 @@
 package truerss.api
 
 import com.github.fntz.omhs.{BodyWriter, CommonResponse}
-import io.netty.handler.codec.http.HttpResponseStatus
+import io.netty.handler.codec.http.{HttpHeaderValues, HttpResponseStatus}
 import io.netty.util.CharsetUtil
 import play.api.libs.json._
 import truerss.dto.Processing
@@ -18,7 +18,6 @@ object HttpApi {
   val notFound = CommonResponse.empty.copy(status = HttpResponseStatus.NOT_FOUND)
   val internal = CommonResponse.empty.copy(status = HttpResponseStatus.INTERNAL_SERVER_ERROR)
 
-  private final val json = "application/json"
   private final val errorF = "errors"
 
   def flush[T](task: Task[T])(implicit writer: BodyWriter[T]): Task[CommonResponse] = {
@@ -33,13 +32,13 @@ object HttpApi {
       case ValidationError(errors) =>
         CommonResponse(
           status = HttpResponseStatus.BAD_REQUEST,
-          contentType = json,
+          contentType = HttpHeaderValues.APPLICATION_JSON.toString,
           content = printErrors(errors).getBytes(CharsetUtil.UTF_8)
         )
       case ContentReadError(error) =>
         CommonResponse(
           status = HttpResponseStatus.INTERNAL_SERVER_ERROR,
-          contentType = json,
+          contentType = HttpHeaderValues.APPLICATION_JSON.toString,
           content = printErrors(error :: Nil).getBytes(CharsetUtil.UTF_8)
         )
       case NotFoundError(_) => notFound
