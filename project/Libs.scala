a3a7127dc5549854a4094586db8b68bb820b9561
mike <mike.fch1@gmail.com>
2021-06-25 01:05:02 +0300

    rm reflect

--- project/Libs.scala
+++ project/Libs.scala
@@ -86,7 +86,7 @@ object Libs {
     "io.netty" % "netty-codec-http2" % "4.1.60.Final",
     "com.github.fntz" %% "omhs-dsl" % "0.0.5",
     "com.github.fntz" %% "omhs-play-support" % "0.0.5",
-    "org.scala-lang" % "scala-reflect" % scalaVersion
+    "org.scala-lang" % "scala-reflect" % scalaVersion % "compile"
   )
 
   val deps = db ++ actorika ++ truerss ++ logs ++
--- src/main/scala/truerss/services/StreamProvider.scala
+++ src/main/scala/truerss/services/StreamProvider.scala
@@ -2,10 +2,10 @@ package truerss.services
 
 import io.truerss.actorika._
 import zio.Task
-import scala.reflect.runtime.universe._
+import scala.reflect.ClassTag
 
 trait StreamProvider { self: Actor =>
-  def fire[T](x: T)(implicit _tag: TypeTag[T]): Task[Unit] = {
+  def fire[T](x: T)(implicit _tag: ClassTag[T]): Task[Unit] = {
     Task.fromFunction { _ =>
       self.system.publish(x)
     }
